7a38d33cada392a23d7154ef6254519cf812463c
Akshay Desai <akshaydesai1675@gmail.com>
2024-02-09 12:50:56 +0530

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-orange-500 text-center font-bold text-2xl">
+      <h1>OTT App</h1>
+    </div>
+  );
+}
+
+export default App;
